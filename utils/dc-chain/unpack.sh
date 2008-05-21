e952bf8269ee83a7610f208caa7b3591a73690db
Cyle Terry <fackue@users.sourceforge.net>
2008-05-21 20:33:31 +0000

    forgot to set a 1.15 back to 1.12

--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf binutils-2.17 gcc-3.4.6 newlib-1.15.0
+rm -rf binutils-2.17 gcc-3.4.6 newlib-1.12.0
 
 tar jxf binutils-2.17.tar.bz2 || exit 1
 tar jxf gcc-3.4.6.tar.bz2 || exit 1
