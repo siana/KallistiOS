73b8f8169994f91e9986fbb4f1532671d3dda963
Harley Laue <lostgeneration@users.sourceforge.net>
2010-04-21 04:44:35 +0000

    Added some doxygen comments to the assert.h and cdrom.h

--- include/assert.h
+++ include/assert.h
@@ -11,6 +11,16 @@
 #include <kos/cdefs.h>
 __BEGIN_DECLS
 
+/**
+	\file assert.h
+	\brief Standard C Assertions
+
+	This file contains the standard C assertions to raise an assertion or to
+	change the assertion handler.
+
+	\author Dan Potter
+*/
+
 /* This is nice and simple, modeled after the BSD one like most of KOS;
    the addition here is assert_msg(), which allows you to provide an
    error message. */
@@ -21,7 +31,25 @@ __BEGIN_DECLS
 #	define assert(e) ((void)0)
 #	define assert_msg(e, m) ((void)0)
 #else
+/**
+	\brief standard C assertion
+
+	\param e a value or expression to be evaluated as true or false
+
+	\return if e is true (void)0 is returned, otherwise, the function does not
+	return and abort() is called
+*/
 #	define assert(e)        ((e) ? (void)0 : __assert(__FILE__, __LINE__, #e, NULL, __FUNCTION__))
+
+/**
+	\brief assert with a custom message
+
+	\param e a value or expression to be evaluated as true or false
+	\param m a const char * message
+
+	\return if e is true (void)0 is returned, otherwise the function does not
+	return, a custom message is displayed, and abort() is called
+*/
 #	define assert_msg(e, m) ((e) ? (void)0 : __assert(__FILE__, __LINE__, #e, m, __FUNCTION__))
 #endif
 
@@ -29,11 +57,32 @@ __BEGIN_DECLS
 void __assert(const char *file, int line, const char *expr,
 	const char *msg, const char *func);
 
-/* Set an "assert handler" which is called whenever an assert happens.
-   By default, this will print a message and call abort(). Returns the old
-   handler's address. */
+/**
+	\brief assert handler
+
+	An assertion handler can be a user defined assertion handler,
+	otherwise the default is used, which calls abort()
+
+	\param file The filename where the assertion happened
+	\param line The line number where the assertion happened
+	\param expr The expression that raised the assertion
+	\param msg A custom message for why the assertion happened
+	\param func The function name from which the assertion happened
+
+	\sa assert_set_handler
+*/
 typedef void (*assert_handler_t)(const char * file, int line, const char * expr,
 	const char * msg, const char * func);
+
+/**
+	\brief set an "assert handler" to call on an assert
+
+	By default, this will print a message and call abort().
+
+	\return the old assert_handler_t address
+
+	\sa assert_handler_t
+*/
 assert_handler_t assert_set_handler(assert_handler_t hnd);
 
 __END_DECLS
--- kernel/arch/dreamcast/include/dc/cdrom.h
+++ kernel/arch/dreamcast/include/dc/cdrom.h
@@ -13,7 +13,9 @@ __BEGIN_DECLS
 
 #include <arch/types.h>
 
-/* Command codes (thanks maiwe) */
+/** \defgroup cmd_codes Command codes (thanks maiwe)
+	@{
+*/
 #define CMD_PIOREAD	16
 #define CMD_DMAREAD	17
 #define CMD_GETTOC	18
@@ -28,27 +30,39 @@ __BEGIN_DECLS
 #define CMD_STOP	33
 #define CMD_GETSCD	34
 #define CMD_GETSES	35
+/** @} */
 
-/* Command responses */
+/** \defgroup cmd_response Command responses
+	@{
+*/
 #define ERR_OK		0
 #define ERR_NO_DISC	1
 #define ERR_DISC_CHG	2
 #define ERR_SYS		3
 #define ERR_ABORTED	4
 #define ERR_NO_ACTIVE	5
+/** @} */
 
-/* Command Status responses */
+/** \defgroup cmd_status_response Command Status responses
+	@{
+*/
 #define FAILED		-1
 #define NO_ACTIVE	0
 #define PROCESSING	1
 #define COMPLETED	2
 #define ABORTED		3
+/** @} */
 
-/* CDDA Read Modes */
+/** \defgroup cdda_read_modes CDDA Read Modes
+	@{
+*/
 #define CDDA_TRACKS	1
 #define CDDA_SECTORS	2
+/** @} */
 
-/* Status values */
+/** \defgroup status_values Status values
+	@{
+*/
 #define CD_STATUS_BUSY		0
 #define CD_STATUS_PAUSED	1
 #define CD_STATUS_STANDBY	2
@@ -57,62 +71,118 @@ __BEGIN_DECLS
 #define CD_STATUS_SCANNING	5
 #define CD_STATUS_OPEN		6
 #define CD_STATUS_NO_DISC	7
+/** @} */
 
-/* Disk types */
+/** \defgroup disk_types Disk types
+	@{
+*/
 #define CD_CDDA		0
 #define CD_CDROM	0x10
 #define CD_CDROM_XA	0x20
 #define CD_CDI		0x30
 #define CD_GDROM	0x80
+/** @} */
 
-/* TOC structure returned by the BIOS */
+/** TOC structure returned by the BIOS */
 typedef struct {
 	uint32	entry[99];
 	uint32	first, last;
 	uint32	leadout_sector;
 } CDROM_TOC;
 
-/* TOC access macros */
+/** \defgroup toc_access TOC access macros
+	@{
+*/
 #define TOC_LBA(n) ((n) & 0x00ffffff)
 #define TOC_ADR(n) ( ((n) & 0x0f000000) >> 24 )
 #define TOC_CTRL(n) ( ((n) & 0xf0000000) >> 28 )
 #define TOC_TRACK(n) ( ((n) & 0x00ff0000) >> 16 )
+/** @} */
 
-/* Sets the sector size */
+/**
+	\brief Sets the sector size
+
+	\param size The size of the sector you're desiring
+*/
 void set_sector_size (int size);
 
-/* Command execution sequence */
+/**
+	\brief Command execution sequence
+
+	\return \ref cmd_response
+*/
 int cdrom_exec_cmd(int cmd, void *param);
 
-/* Return the status of the drive as two integers (see constants) */
+/**
+	\brief Gets the status of the the cdrom
+	\return the status of the drive as two integers (see constants)
+*/
 int cdrom_get_status(int *status, int *disc_type);
 
-/* Re-init the drive, e.g., after a disc change, etc */
+/**
+	\brief Re-init the drive, e.g., after a disc change, etc
+
+	\return \ref cmd_response
+*/
 int cdrom_reinit();
 
-/* Read the table of contents */
+/**
+	\brief Read the table of contents
+
+	\return \ref cmd_response
+*/
 int cdrom_read_toc(CDROM_TOC *toc_buffer, int session);
 
-/* Read one or more sectors */
+/**
+	\brief Read one or more sector
+
+	\return \ref cmd_response
+*/
 int cdrom_read_sectors(void *buffer, int sector, int cnt);
 
-/* Locate the LBA sector of the data track */
+/**
+	\brief Locate the LBA sector of the data track
+*/
 uint32 cdrom_locate_data_track(CDROM_TOC *toc);
 
-/* Play CDDA audio tracks or sectors */
+/**
+	\brief Play CDDA audio tracks or sectors
+
+	\return \ref cmd_response
+*/
 int cdrom_cdda_play(uint32 start, uint32 end, uint32 loops, int mode);
 
-/* Pause CDDA audio playback */
+/*
+	\brief Pause CDDA audio playback
+
+	\return \ref cmd_response
+*/
 int cdrom_cdda_pause();
 
-/* Resume CDDA audio playback */
+/**
+	\brief Resume CDDA audio playback
+
+	\return \ref cmd_response
+*/
 int cdrom_cdda_resume();
 
-/* Spin down the CD */
+/**
+	\brief Spin down the CD
+
+	\return \ref cmd_response
+*/
 int cdrom_spin_down();
 
-/* Initialize */
+/**
+	\brief Initialize cdrom
+
+	\return -1 if cdrom_init has already been called, otherwise 0
+*/
 int cdrom_init();
+
+/**
+	\brief Shutdown cdrom
+*/
 void cdrom_shutdown();
 
 __END_DECLS
