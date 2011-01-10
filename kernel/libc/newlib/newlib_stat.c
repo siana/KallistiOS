94bfd08ad44b8d651a03893da6011af8ac00ec76
Lawrence Sebald <ljsebald@users.sourceforge.net>
2011-01-10 05:46:04 +0000

    This is a little bit better version of stat(). At least this one will work for a few common use-cases of it...

--- kernel/libc/newlib/newlib_stat.c
+++ kernel/libc/newlib/newlib_stat.c
@@ -1,14 +1,40 @@
 /* KallistiOS ##version##
 
    newlib_stat.c
-   Copyright (C)2004 Dan Potter
+   Copyright (C) 2004 Dan Potter
+   Copyright (C) 2011 Lawrence Sebald
 
 */
 
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/reent.h>
+#include <kos/fs.h>
+#include <errno.h>
 
-int _stat_r(void * reent, const char * fn, struct stat * pstat) {
-	pstat->st_mode = S_IFCHR;
-	return 0;
+int _stat_r(struct _reent * reent, const char * fn, struct stat * pstat) {
+    file_t fp = fs_open(fn, O_RDONLY);
+    mode_t md = S_IFREG;
+
+    /* If we couldn't get it as a file, try as a directory */
+    if(fp == FILEHND_INVALID) {
+        fp = fs_open(fn, O_RDONLY | O_DIR);
+        md = S_IFDIR;
+    }
+
+    /* If we still don't have it, then we're not going to get it. */
+    if(fp == FILEHND_INVALID) {
+        reent->_errno = ENOENT;
+        return -1;
+    }
+
+    /* This really doesn't convey all that much information, but it should help
+       with at least some uses of stat. */
+    pstat->st_mode = md;
+    pstat->st_size = (off_t)fs_total(fp);
+
+    /* Clean up after ourselves. */
+    fs_close(fp);
+
+    return 0;
 }
