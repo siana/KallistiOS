15e46331ddfb0ee89f328a6ed7697b4872606652
Lawrence Sebald <ljsebald@users.sourceforge.net>
2011-05-07 02:22:41 +0000

    Add in some more doxygen comments...

--- kernel/arch/dreamcast/include/dc/net/lan_adapter.h
+++ kernel/arch/dreamcast/include/dc/net/lan_adapter.h
@@ -1,10 +1,19 @@
 /* KallistiOS ##version##
- *
- * dc/net/lan_adapter.h
- *
- * (c)2002 Dan Potter
- *
- */
+
+   dc/net/lan_adapter.h
+   Copyright (C) 2002 Dan Potter
+
+*/
+
+/** \file   dc/net/lan_adapter.h
+    \brief  LAN Adapter support.
+
+    This file contains declarations related to support for the HIT-0300 "LAN
+    Adapter". There's not really anything that users will generally have to deal
+    with in here.
+
+    \author Dan Potter
+*/
 
 #ifndef __DC_NET_LAN_ADAPTER_H
 #define __DC_NET_LAN_ADAPTER_H
@@ -14,11 +23,13 @@ __BEGIN_DECLS
 
 #include <kos/net.h>
 
+/* \cond */
 /* Initialize */
 int la_init();
 
 /* Shutdown */
 int la_shutdown();
+/* \endcond */
 
 __END_DECLS
 
