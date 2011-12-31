815390099fcc5a72e230df9c1149ab184843b3e0
Lawrence Sebald <ljsebald@users.sourceforge.net>
2011-12-31 18:10:30 +0000

    Binutils 2.21 seems to have disappeared from the GNU FTP site, so use 2.22 instead.

--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf binutils-2.21 gcc-4.5.2 newlib-1.19.0
+rm -rf binutils-2.22 gcc-4.5.2 newlib-1.19.0
 
-tar jxf binutils-2.21.tar.bz2 || exit 1
+tar jxf binutils-2.22.tar.bz2 || exit 1
 tar jxf gcc-4.5.2.tar.bz2 || exit 1
 tar zxf newlib-1.19.0.tar.gz || exit 1
