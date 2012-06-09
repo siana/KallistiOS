535ce237c205ffe5e96a72d11c89ec4813c87bb4
Lawrence Sebald <ljsebald@users.sourceforge.net>
2012-06-09 23:16:30 +0000

    Revert to GCC 4.5.2 for now, due to issue with GCC 4.7.0 and frame pointers on sh-elf. See GCC bug for more info: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53621

--- utils/dc-chain/download.sh
+++ utils/dc-chain/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2 || exit 1
-wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.0/gcc-4.7.0.tar.bz2 || exit 1
+wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.5.2/gcc-4.5.2.tar.bz2 || exit 1
 wget -c ftp://sources.redhat.com/pub/newlib/newlib-1.20.0.tar.gz || exit 1
 
--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf binutils-2.22 gcc-4.7.0 newlib-1.20.0
+rm -rf binutils-2.22 gcc-4.5.2 newlib-1.20.0
 
 tar jxf binutils-2.22.tar.bz2 || exit 1
-tar jxf gcc-4.7.0.tar.bz2 || exit 1
+tar jxf gcc-4.5.2.tar.bz2 || exit 1
 tar zxf newlib-1.20.0.tar.gz || exit 1
