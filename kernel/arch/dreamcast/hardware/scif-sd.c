1d64aea428149ecccdadbd939862052507439d09
Lawrence Sebald <ljsebald@users.sourceforge.net>
2012-11-29 16:53:23 -0500

    Add in low-level support for the SD card reader attached to the serial port.

---
+++ kernel/arch/dreamcast/hardware/scif-sd.c
@@ -0,0 +1,224 @@
+/* KallistiOS ##version##
+
+   hardware/scif-sd.c
+   Copyright (C) 2012 Lawrence Sebald
+*/
+
+#include <dc/scif.h>
+#include <dc/fs_dcload.h>
+#include <arch/timer.h>
+#include <kos/dbglog.h>
+
+/* SCIF registers */
+#define SCIFREG08(x) *((volatile uint8 *)(x))
+#define SCIFREG16(x) *((volatile uint16 *)(x))
+#define SCSMR2  SCIFREG16(0xffe80000)
+#define SCBRR2  SCIFREG08(0xffe80004)
+#define SCSCR2  SCIFREG16(0xffe80008)
+#define SCFTDR2 SCIFREG08(0xffe8000C)
+#define SCFSR2  SCIFREG16(0xffe80010)
+#define SCFRDR2 SCIFREG08(0xffe80014)
+#define SCFCR2  SCIFREG16(0xffe80018)
+#define SCFDR2  SCIFREG16(0xffe8001C)
+#define SCSPTR2 SCIFREG16(0xffe80020)
+#define SCLSR2  SCIFREG16(0xffe80024)
+
+/* Values for the SCSPTR2 register */
+#define PTR2_RTSIO  (1 << 7)
+#define PTR2_RTSDT  (1 << 6)
+#define PTR2_CTSIO  (1 << 5)
+#define PTR2_CTSDT  (1 << 4)
+#define PTR2_SPB2IO (1 << 1)
+#define PTR2_SPB2DT (1 << 0)
+
+/* This doesn't seem to actually be necessary on any of the SD cards I've tried,
+   but I'm keeping it around, just in case... */
+#define SD_WAIT() asm("nop\n\tnop\n\tnop\n\tnop\n\tnop")
+
+static uint16 scsptr2 = 0;
+
+/* Re-initialize the state of SCIF to match what we need for communication with
+   the SD card. We basically take complete control of the pins of the port
+   directly, overriding the normal byte FIFO and whatnot. */
+int scif_sd_init(void) {
+    /* Make sure we're not using dcload-serial. If we are, then we definitely do
+       not have a SD card on the serial port. */
+    if(*DCLOADMAGICADDR == DCLOADMAGICVALUE && dcload_type == DCLOAD_TYPE_SER) {
+        dbglog(DBG_KDEBUG, "scif_sd_init: no SD card -- using dcload-serial\n");
+        return -1;
+    }
+
+    /* Clear most of the registers, since we're going to do all the hard work in
+       software anyway... */
+    SCSCR2 = 0;
+    SCFCR2 = 0x06;                          /* Empty the FIFOs */
+    SCFCR2 = 0;
+    SCSMR2 = 0;
+    SCFSR2 = 0;
+    SCLSR2 = 0;
+    SCSPTR2 = scsptr2 = PTR2_RTSIO | PTR2_RTSDT | PTR2_CTSIO | PTR2_SPB2IO;
+
+    return 0;
+}
+
+int scif_sd_shutdown(void) {
+    return 0;
+}
+
+void scif_sd_set_cs(int v) {
+    if(v)
+        scsptr2 |= PTR2_RTSDT;
+    else
+        scsptr2 &= ~PTR2_RTSDT;
+    SCSPTR2 = scsptr2;
+}
+
+uint8 scif_sd_rw_byte(uint8 b) {
+    uint16 tmp = scsptr2 & ~PTR2_CTSDT & ~PTR2_SPB2DT;
+    uint8 bit;
+    uint8 rv = 0;
+
+    /* Write the data out, one bit at a time (most significant bit first), while
+       reading in a data byte, one bit at a time as well...
+       For some reason, we have to have the bit set on the Tx line before we set
+       CTS, otherwise it doesn't work -- that's why this looks so ugly... */
+    SCSPTR2 = tmp | (bit = (b >> 7) & 0x01);    /* write 7 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = SCSPTR2 & PTR2_CTSDT;                  /* read 7 */
+    SCSPTR2 = tmp | (bit = (b >> 6) & 0x01);    /* write 6 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 6 */
+    SCSPTR2 = tmp | (bit = (b >> 5) & 0x01);    /* write 5 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 5 */
+    SCSPTR2 = tmp | (bit = (b >> 4) & 0x01);    /* write 4 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 4 */
+    SCSPTR2 = tmp | (bit = (b >> 3) & 0x01);    /* write 3 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 3 */
+    SCSPTR2 = tmp | (bit = (b >> 2) & 0x01);    /* write 2 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 2 */
+    SCSPTR2 = tmp | (bit = (b >> 1) & 0x01);    /* write 1 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 1 */
+    SCSPTR2 = tmp | (bit = (b >> 0) & 0x01);    /* write 0 */
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* read 0 */
+    SCSPTR2 = tmp;
+
+    return rv;
+}
+
+/* Very accurate 1.5usec delay... */
+static void slow_rw_delay(void) {
+    timer_prime(TMU1, 2000000, 0);
+    timer_clear(TMU1);
+    timer_start(TMU1);
+
+    while(!timer_clear(TMU1)) ;
+    while(!timer_clear(TMU1)) ;
+    while(!timer_clear(TMU1)) ;
+    timer_stop(TMU1);
+}
+
+uint8 scif_sd_slow_rw_byte(uint8 b) {
+    int i;
+    uint8 rv = 0;
+    uint16 tmp = scsptr2 & ~PTR2_CTSDT & ~PTR2_SPB2DT;
+    uint8 bit;
+
+    for(i = 7; i >= 0; --i) {
+        SCSPTR2 = tmp | (bit = (b >> i) & 0x01);
+        slow_rw_delay();
+        SCSPTR2 = tmp | bit | PTR2_CTSDT;
+        rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);
+        slow_rw_delay();
+    }
+
+    return rv;
+}
+
+void scif_sd_write_byte(uint8 b) {
+    uint16 tmp = scsptr2 & ~PTR2_CTSDT & ~PTR2_SPB2DT;
+    uint8 bit;
+
+    /* Write the data out, one bit at a time (most significant bit first)...
+       For some reason, we have to have the bit set on the Tx line before we set
+       CTS, otherwise it doesn't work -- that's why this looks so ugly... */
+    SCSPTR2 = tmp | (bit = (b >> 7) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 6) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 5) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 4) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 3) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 2) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 1) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp | (bit = (b >> 0) & 0x01);
+    SCSPTR2 = tmp | bit | PTR2_CTSDT;
+    SD_WAIT();
+    SCSPTR2 = tmp;
+}
+
+uint8 scif_sd_read_byte(void) {
+    uint8 rv = 0;
+    uint16 tmp;
+
+    /* Read the data in, one bit at a time (most significant bit first) */
+    SCSPTR2 = tmp = scsptr2 | PTR2_SPB2DT | PTR2_CTSDT;
+    SD_WAIT();
+    rv = SCSPTR2 & PTR2_SPB2DT;                 /* 7 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 6 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 5 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 4 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 3 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 2 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 1 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+    SCSPTR2 = tmp;
+    SD_WAIT();
+    rv = (rv << 1) | (SCSPTR2 & PTR2_SPB2DT);   /* 0 */
+    SCSPTR2 = tmp & ~PTR2_CTSDT;
+
+    return rv;
+}
--- kernel/arch/dreamcast/include/dc/scif.h
+++ kernel/arch/dreamcast/include/dc/scif.h
@@ -2,6 +2,7 @@
 
    dc/scif.h
    Copyright (C) 2000,2001,2004 Dan Potter
+   Copyright (C) 2012 Lawrence Sebald
 
 */
 
@@ -11,6 +12,7 @@
     This file deals with raw access to the serial port on the Dreamcast.
 
     \author Dan Potter
+    \author Lawrence Sebald
 */
 
 #ifndef __DC_SCIF_H
@@ -107,6 +109,79 @@ int scif_read_buffer(uint8 *data, int len);
 
 /** \brief  SCIF debug I/O handler. Do not modify! */
 extern dbgio_handler_t dbgio_scif;
+
+/* Low-level SD Card related functionality below here... */
+/** \brief  Initialize the SCIF port for use of the SD card adapter.
+
+    This function initializes the SCIF port for accessing the an SD card that
+    has been connected to the serial port. The design of the adapter follows
+    that which is (at least now) somewhat commonly available online and is the
+    same as the one designed by jj1odm.
+
+    \retval 0               On success.
+    \retval -1              On error (if dcload-serial is detected).
+*/
+int scif_sd_init(void);
+
+/** \brief  Shut down SD card support over the SCIF port.
+
+    This function shuts down SD card support on the SCIF port. If you want to
+    get regular usage of the port back, you must call scif_init() after shutting
+    down the SD card support.
+
+    \retval 0               On success (no errors defined).
+*/
+int scif_sd_shutdown(void);
+
+/** \brief  Set or clear the SD card /CS line.
+
+    This function sets or clears the /CS line (connected to the RTS line of the
+    SCIF port).
+
+    \param  v               Non-zero to output 1 on the line, zero to output 0.
+*/
+void scif_sd_set_cs(int v);
+
+/** \brief  Read and write one byte from the SD card.
+
+    This function writes one byte and reads one back from the SD card
+    simultaneously.
+
+    \param  b               The byte to write out to the port.
+    \retval                 The byte returned from the card.
+*/
+uint8 scif_sd_rw_byte(uint8 b);
+
+/** \brief  Read and write one byte from the SD card, slowly.
+
+    This function does the same thing as the scif_sd_rw_byte() function, but
+    with a 1.5usec delay between asserting the CLK line and reading back the bit
+    and a 1.5usec delay between clearing the CLK line and writing the next bit
+    out.
+
+    \param  b               The byte to write out to the port.
+    \retval                 The byte returned from the card.
+*/
+uint8 scif_sd_slow_rw_byte(uint8 b);
+
+
+/** \brief  Write a byte to the SD card.
+
+    This function writes out the specified byte to the SD card, one bit at a
+    time. The timing follows that of the scif_sd_rw_byte() function.
+
+    \param  b               The byte to write out to the port.
+*/
+void scif_sd_write_byte(uint8 b);
+
+/** \brief  Read a byte from the SD card.
+
+    This function reads a byte from the SD card, one bit at a time. The timing
+    follows that of the scif_sd_rw_byte() function.
+
+    \retval                 The byte returned from the card.
+*/
+uint8 scif_sd_read_byte(void);
 
 __END_DECLS
 
