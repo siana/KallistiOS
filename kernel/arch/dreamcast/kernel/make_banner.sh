e29e15ea9864b2a6fff2df5d8f75d4ac6c590468
Lawrence Sebald <ljsebald@users.sourceforge.net>
2013-04-02 10:56:44 -0400

    Use the git commit hash in the make_banner.sh script.

--- kernel/arch/dreamcast/kernel/make_banner.sh
+++ kernel/arch/dreamcast/kernel/make_banner.sh
@@ -5,9 +5,9 @@
 echo 'char banner[] = ' > banner.c
 
 echo -n '"KallistiOS ' >> banner.c
-if [ -d ".svn" ]; then
-    echo -n 'SVN r' >> banner.c
-    echo -n `svnversion` >> banner.c
+if [ -d "$KOS_BASE/.git" ]; then
+    echo -n 'Git revision ' >> banner.c
+    echo -n `git rev-list --full-history --all --abbrev-commit | head -1` >> banner.c
     echo -n ': ' >> banner.c
 else
     echo -n '##version##: ' >> banner.c
