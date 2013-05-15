d793fac0b2d160ba5e9b4501d5c89a13c12c740d
Christian Groessler <chris@groessler.org>
2013-05-15 09:56:31 +0200

    New compiler (gcc 4.7.0) optimizes out the the delay loop in aica_get_pos().  Add a inline "nop" to prevent this.

--- kernel/arch/dreamcast/sound/arm/aica.c
+++ kernel/arch/dreamcast/sound/arm/aica.c
@@ -221,7 +221,8 @@ int aica_get_pos(int ch) {
     SNDREG8(0x280d) = ch;
 
     /* Wait a while */
-    for(i = 0; i < 20; i++);
+    for(i = 0; i < 20; i++)
+        __asm__ volatile ("nop");  /* Prevent loop from being optimized out */
 
     /* Update position counters */
     chans[ch].pos = SNDREG32(0x2814) & 0xffff;
