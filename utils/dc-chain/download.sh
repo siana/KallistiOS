326499749579f54a820ad565627aa80ed6a52ae6
Lawrence Sebald <ljsebald@users.sourceforge.net>
2013-05-18 13:32:54 -0400

    A few dc-chain updates: 1. Update the packages to the following versions:    Binutils 2.23.2    GCC 4.7.3    Newlib 2.0.0 2. Add a makejobs variable to the Makefile to allow multiple jobs to be spawned for building the tools. 3. Fix an issue that would cause the Makefile to not fail when verbose was set to 1 and one of the jobs spawned failed. This is probably the most often reported issue with this thing...

--- utils/dc-chain/download.sh
+++ utils/dc-chain/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2 || exit 1
-wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.0/gcc-4.7.0.tar.bz2 || exit 1
-wget -c ftp://sources.redhat.com/pub/newlib/newlib-1.20.0.tar.gz || exit 1
+wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2 || exit 1
+wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.3/gcc-4.7.3.tar.bz2 || exit 1
+wget -c ftp://sources.redhat.com/pub/newlib/newlib-2.0.0.tar.gz || exit 1
 
