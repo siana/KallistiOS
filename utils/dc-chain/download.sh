7f4c1c5aa6c4b8dce6fc98bfd0f161a5ff7d2492
Lawrence Sebald <ljsebald@users.sourceforge.net>
2013-11-17 21:38:46 -0500

    A few changes/updates to dc-chain: 1. Update GCC version to 4.8.2. 2. Automatically build GCC's dependencies along with GCC. 3. Add in a small fix for OS X Mavericks (GCC can't be built against libc++).

--- utils/dc-chain/download.sh
+++ utils/dc-chain/download.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
-wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2 || exit 1
-wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.3/gcc-4.7.3.tar.bz2 || exit 1
-wget -c ftp://sourceware.org/pub/newlib/newlib-2.0.0.tar.gz || exit 1
 
+# These version numbers are all that should ever have to be changed.
+export GCC_VER=4.8.2
+export BINUTILS_VER=2.23.2
+export NEWLIB_VER=2.0.0
+export GMP_VER=5.1.3
+export MPFR_VER=3.1.2
+export MPC_VER=1.0.1
+
+# Download everything.
+if command -v wget >/dev/null 2>&1; then
+    echo "Downloading binutils-$BINUTILS_VER..."
+    wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.bz2 || exit 1
+    echo "Downloading GCC $GCC_VER..."
+    wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
+    echo "Downloading Newlib $NEWLIB_VER..."
+    wget -c ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.gz || exit 1
+    echo "Downloading GMP $GMP_VER..."
+    wget -c ftp://ftp.gnu.org/gnu/gmp/gmp-$GMP_VER.tar.bz2 || exit 1
+    echo "Downloading MPFR $MPFR_VER..."
+    wget -c http://www.mpfr.org/mpfr-current/mpfr-$MPFR_VER.tar.bz2 || exit 1
+    echo "Downloading MPC $MPC_VER..."
+    wget -c http://www.multiprecision.org/mpc/download/mpc-$MPC_VER.tar.gz || exit 1
+elif command -v curl >/dev/null 2>&1; then
+    echo "Downloading Binutils $BINUTILS_VER..."
+    curl -C - -O ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.bz2 || exit 1
+    echo "Downloading GCC $GCC_VER..."
+    curl -C - -O ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
+    echo "Downloading Newlib $NEWLIB_VER..."
+    curl -C - -O ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.gz || exit 1
+    echo "Downloading GMP $GMP_VER..."
+    curl -C - -O ftp://ftp.gnu.org/gnu/gmp/gmp-$GMP_VER.tar.bz2 || exit 1
+    echo "Downloading MPFR $MPFR_VER..."
+    curl -O http://www.mpfr.org/mpfr-current/mpfr-$MPFR_VER.tar.bz2 || exit 1
+    echo "Downloading MPC $MPC_VER..."
+    curl -O http://www.multiprecision.org/mpc/download/mpc-$MPC_VER.tar.gz || exit 1
+else
+    echo >&2 "You must have either wget or cURL installed to use this script!"
+    exit 1
+fi
--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-rm -rf binutils-2.23.2 gcc-4.7.3 newlib-2.0.0
+# These version numbers are all that should ever have to be changed.
+export GCC_VER=4.8.2
+export BINUTILS_VER=2.23.2
+export NEWLIB_VER=2.0.0
+export GMP_VER=5.1.3
+export MPFR_VER=3.1.2
+export MPC_VER=1.0.1
 
-tar jxf binutils-2.23.2.tar.bz2 || exit 1
-tar jxf gcc-4.7.3.tar.bz2 || exit 1
-tar zxf newlib-2.0.0.tar.gz || exit 1
+# Clean up from any old builds.
+rm -rf binutils-$BINUTILS_VER gcc-$GCC_VER newlib-$NEWLIB_VER
+rm -rf gmp-$GMP_VER mpfr-$MPFR_VER mpc-$MPC_VER
+
+# Unpack everything.
+tar jxf binutils-$BINUTILS_VER.tar.bz2 || exit 1
+tar jxf gcc-$GCC_VER.tar.bz2 || exit 1
+tar zxf newlib-$NEWLIB_VER.tar.gz || exit 1
+tar jxf gmp-$GMP_VER.tar.bz2 || exit 1
+tar jxf mpfr-$MPFR_VER.tar.bz2 || exit 1
+tar zxf mpc-$MPC_VER.tar.gz || exit 1
+
+# Move the GCC dependencies into their required locations.
+mv gmp-$GMP_VER gcc-$GCC_VER/gmp
+mv mpfr-$MPFR_VER gcc-$GCC_VER/mpfr
+mv mpc-$MPC_VER gcc-$GCC_VER/mpc
