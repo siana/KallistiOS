abd905c6a87e888d0abef384e83acfdbb7df339d
Lawrence Sebald <ljsebald@users.sourceforge.net>
2014-02-17 09:42:24 -0500

    dc-chain: Rolling back, once again, to GCC 4.7.3 due to some performance regressions in 4.8.x. Also, added a flag to the download.sh and unpack.sh scripts to not download/set up the GCC dependencies (in case you install them separately).

--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -1,12 +1,37 @@
 #!/bin/sh
 
 # These version numbers are all that should ever have to be changed.
-export GCC_VER=4.8.2
+export GCC_VER=4.7.3
 export BINUTILS_VER=2.23.2
 export NEWLIB_VER=2.0.0
 export GMP_VER=5.1.3
 export MPFR_VER=3.1.2
 export MPC_VER=1.0.1
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    case $PARAM in
+        --no-gmp)
+            unset GMP_VER
+            ;;
+        --no-mpfr)
+            unset MPFR_VER
+            ;;
+        --no-mpc)
+            unset MPC_VER
+            ;;
+        --no-deps)
+            unset GMP_VER
+            unset MPFR_VER
+            unset MPC_VER
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 # Clean up from any old builds.
 rm -rf binutils-$BINUTILS_VER gcc-$GCC_VER newlib-$NEWLIB_VER
@@ -16,11 +41,19 @@ rm -rf gmp-$GMP_VER mpfr-$MPFR_VER mpc-$MPC_VER
 tar jxf binutils-$BINUTILS_VER.tar.bz2 || exit 1
 tar jxf gcc-$GCC_VER.tar.bz2 || exit 1
 tar zxf newlib-$NEWLIB_VER.tar.gz || exit 1
-tar jxf gmp-$GMP_VER.tar.bz2 || exit 1
-tar jxf mpfr-$MPFR_VER.tar.bz2 || exit 1
-tar zxf mpc-$MPC_VER.tar.gz || exit 1
 
-# Move the GCC dependencies into their required locations.
-mv gmp-$GMP_VER gcc-$GCC_VER/gmp
-mv mpfr-$MPFR_VER gcc-$GCC_VER/mpfr
-mv mpc-$MPC_VER gcc-$GCC_VER/mpc
+# Unpack the GCC dependencies and move them into their required locations.
+if [ -n "$GMP_VER" ]; then
+    tar jxf gmp-$GMP_VER.tar.bz2 || exit 1
+    mv gmp-$GMP_VER gcc-$GCC_VER/gmp
+fi
+
+if [ -n "$MPFR_VER" ]; then
+    tar jxf mpfr-$MPFR_VER.tar.bz2 || exit 1
+    mv mpfr-$MPFR_VER gcc-$GCC_VER/mpfr
+fi
+
+if [ -n "$MPC_VER" ]; then
+    tar zxf mpc-$MPC_VER.tar.gz || exit 1
+    mv mpc-$MPC_VER gcc-$GCC_VER/mpc
+fi
