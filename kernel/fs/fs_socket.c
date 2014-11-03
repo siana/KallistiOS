41725dd391b0625b72b19e57a4edef3e33457b62
Lawrence Sebald <ljsebald@users.sourceforge.net>
2014-11-03 14:41:45 -0500

    Add an initializer for rewinddir for fs_socket.

--- kernel/fs/fs_socket.c
+++ kernel/fs/fs_socket.c
@@ -126,7 +126,8 @@ static vfs_handler_t vh = {
     NULL,            /* seek64 */
     NULL,            /* tell64 */
     NULL,            /* total64 */
-    NULL             /* readlink */
+    NULL,            /* readlink */
+    NULL             /* rewinddir */
 };
 
 /* Have we been initialized? */
